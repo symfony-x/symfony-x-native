a00bcff3628a026f772c116c975c1501c3ab202b
xor <xorman@gmail.com>
2024-11-11 21:03:33 -0500

    [dev] wip

--- importmap.php
+++ importmap.php
@@ -16,14 +16,14 @@ return [
         'path' => './assets/app.js',
         'entrypoint' => true,
     ],
-    '@hotwired/stimulus' => [
-        'version' => '3.2.2',
-    ],
     '@symfony/stimulus-bundle' => [
         'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
     ],
+    '@hotwired/stimulus' => [
+        'version' => '3.2.2',
+    ],
     '@hotwired/turbo' => [
-        'version' => '7.3.0',
+        'version' => '8.0.12',
     ],
     'bootstrap' => [
         'version' => '5.3.3',
