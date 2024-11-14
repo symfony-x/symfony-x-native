e4e11ddfdb01d1a57f07971c9f483c331b91c2a3
xor <xorman@gmail.com>
2024-11-13 23:29:04 -0500

    [dev] oops

--- rename.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Define the target directory
-TARGET_DIR="dev/bootstrap-examples/"
-
-# Find all .html files and rename them to .phtml
-find "$TARGET_DIR" -type f -name "*.html" | while read -r file; do
-    mv -- "$file" "${file%.html}.phtml"
-done
